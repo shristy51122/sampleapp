823d152c66a2df134f63f5ada52b5ce81baac6c6
shristy51122 <shriskum@LIN51015445.corp.capgemini.com>
2021-09-17 15:59:48 +0530

    my first commit

---
+++ sample/src/sample/boolDemo.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class boolDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!! this is STS");
+
+	}
+
+}
